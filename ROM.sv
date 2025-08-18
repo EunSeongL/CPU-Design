d96b4403a4388f1509744169aafb3eaa8307dca1
EunSeongL <128117825+EunSeongL@users.noreply.github.com>
2025-08-18 21:39:26 +0900

    Add files via upload

---
+++ ROM.sv
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module ROM (
+    input  logic [31:0] addr,
+    output logic [31:0] data
+);
+    logic [31:0] rom[0:61];
+    
+    assign data = rom[addr[31:2]];
+
+endmodule
